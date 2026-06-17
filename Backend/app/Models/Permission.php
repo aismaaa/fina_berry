3d1da9c10f80bc3df1a34e07b383465b5bcb6be5
mfajarrr3004-boop <mfajarrr3004@gmail.com>
2026-06-17 17:33:35 +0700

    closes #2 Login API , closes #3 Middleware Auth , closes #4 Role management , closes #5 Dashboard API

---
+++ Backend/app/Models/Permission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $fillable = ['name', 'description'];
+
+    /**
+     * Get the roles that have this permission.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permissions', 'permission_id', 'role_id');
+    }
+}
